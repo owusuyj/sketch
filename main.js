d4d1c09dc29e9c022532559a953add7864b5c86d
owusuyj <yawjnr@vt.edu>
2023-06-21 02:18:58 -0500

    Add functionality of hover

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const mainContainer = document.querySelector(".mainContainer");
-for(x=0; x<9;x++){
-    var grid = document.createElement('div');
+for(x=0; x<100;x++){
+    const grid = document.createElement('div');
+    grid.className = "grid";
+    grid.style.cssText = "background-color: black; width: 15px; height: 15px;"
+    mainContainer.append(grid);
 }
+
+const grids = document.querySelectorAll(".grid");
+grids.forEach(x => x.addEventListener("mouseover", () => {
+    x.classList.toggle("over");
+}))
